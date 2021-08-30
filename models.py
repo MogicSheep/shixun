0908fe38dbc54f7bac5f53173ba3db95ce7beca2
田原 <tahara@tianyuandeMacBook-Pro.local>
2021-08-30 16:39:46 +0800

    database models init

--- models.py
+++ models.py
@@ -1,49 +1,84 @@
-from sqlalchemy import Column, String, create_engine,Integer,ForeignKey
+# coding: utf-8
 from flask_sqlalchemy import SQLAlchemy
-import json
-import os
-database_name="shixun"
-database_path = os.environ.get('DATABASE_URL',"mysql://{}:{}@{}/{}".format('yxn', 'bilibili','118.195.233.143:3306', database_name))
+
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    db.app = app
-    db.init_app(app)
-    db.create_all()
 
 
-class Marbre(db.Model):
-    id = Column(Integer(), primary_key=True)
-    title = Column(String(80))
-    image = Column(String(200))
-    price = Column(Integer())
-    origin = Column(String(80))
-    description = Column(String(500),nullable=True)
+class Addres(db.Model):
+    __tablename__ = 'address'
 
-    def insert(self):
-        db.session.add(self)
-        db.session.commit()
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    content = db.Column(db.String(200), nullable=False, server_default=db.FetchedValue())
+    createat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updateat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    deleteat = db.Column(db.DateTime)
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
 
-    def update(self):
-        db.session.commit()
-    
-    def format(self):
-        return {
-        'id': self.id,
-        'title': self.title,
-        'image': self.image,
-        'price': self.price,
-        'origin': self.origin,
-        'description':self.description,
-        }
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    commodity = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    content = db.Column(db.String(500), nullable=False, server_default=db.FetchedValue())
+    createat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updateat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    deleteat = db.Column(db.DateTime)
+
+
+
+class Commodity(db.Model):
+    __tablename__ = 'commodity'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    title = db.Column(db.String(50), nullable=False, server_default=db.FetchedValue())
+    content = db.Column(db.String(500), nullable=False, server_default=db.FetchedValue())
+    tag = db.Column(db.String(500), nullable=False, server_default=db.FetchedValue())
+    createat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updateat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    deleteat = db.Column(db.DateTime)
+
+
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+    commodity = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    createat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updateat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    deleteat = db.Column(db.DateTime)
+
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+
+    id = db.Column(db.Integer, primary_key=True)
+    courier = db.Column(db.String(50), nullable=False, server_default=db.FetchedValue())
+    status = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    seller = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    buyer = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    commodity = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    createat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updateat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    deleteat = db.Column(db.DateTime)
+
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50, 'utf8_general_ci'), nullable=False, server_default=db.FetchedValue())
+    phone = db.Column(db.BigInteger, nullable=False)
+    region = db.Column(db.String(50), nullable=False, server_default=db.FetchedValue())
+    pwd = db.Column(db.String(50))
+    createat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updateat = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    deleteat = db.Column(db.DateTime)
