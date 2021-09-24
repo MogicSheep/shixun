ccccba991607d08f8fc840783da365e4181bcf6a
YuanSnowing <178853801@qq.com>
2021-09-24 13:16:09 +0800

    logger turn to warning

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import logging
+import pickle
 import os
-from flask import Flask,jsonify,request,abort, redirect, render_template, url_for, flash
+from flask import Flask, jsonify, request, abort, redirect, render_template, url_for, flash
 from werkzeug.wrappers import PlainRequest
 from flask_cors import CORS
-from models import db, Address,User, setup_db, Commodity, Image
+from models import db, Address, User, setup_db, Commodity, Image
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_login import LoginManager
 
@@ -11,13 +13,11 @@ from blueprints.product import product_bp
 from blueprints.order import order_bp
 from blueprints.search import search_bp
 from blueprints.media import media_bp
-from blueprints.recommend import recommend_bp 
+from blueprints.recommend import recommend_bp
 
 import pymysql
 pymysql.install_as_MySQLdb()
 
-import pickle
-import logging
 
 def create_app(test_config=None):
 
@@ -28,13 +28,14 @@ def create_app(test_config=None):
     @app.route('/')
     def get_test():
         return jsonify({
-            'success':True,
+            'success': True,
             'marbre': 'welcome'
         })
-    
+
     register_blueprints(app)
 
     return app
+
 
 def register_blueprints(app):
     app.register_blueprint(user_bp)
@@ -45,7 +46,6 @@ def register_blueprints(app):
     app.register_blueprint(recommend_bp)
 
 
-
 app = create_app()
 app.debug = True
 app.config['DEBUG'] = True
@@ -54,12 +54,16 @@ app.config["SECRET_KEY"] = 'TPmi4aLWRbyVq8zu9v82dWYW1'
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 
-logging.basicConfig(level = logging.DEBUG,format = '%(asctime)s - %(name)s - [%(levelname)s]: %(message)s')
+logging.basicConfig(level=logging.WARNING, 
+                    format='%(asctime)s - %(name)s - [%(levelname)s]: %(message)s', 
+                    filename='log.txt',
+                    filemode='a',)
 logger = logging.getLogger(__name__)
 logger.info('Init finished!')
 
